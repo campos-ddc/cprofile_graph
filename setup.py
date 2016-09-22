be96c86b2a823dbc915c995ad2b806f5e8c71cbb
Diogo de Campos <diogo@unity3d.com>
2016-09-22 17:21:22 +0200

    Updated Pypi development status

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     zip_safe=False,
     keywords='cprofile_graph',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
