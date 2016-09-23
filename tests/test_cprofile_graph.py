a97f51cdc685b1988d02552827bfe92b89977dcd
Diogo de Campos <diogo@unity3d.com>
2016-09-23 14:33:05 +0200

    Added `profile_decorator`
    Moved code into `cprofile_graph.__init__.py`
    
    Added `pytest_mock` dev requirement

--- tests/test_cprofile_graph.py
+++ tests/test_cprofile_graph.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from cprofile_graph import profile_ctx, profile_code, profile_file
+from cprofile_graph import profile_ctx, profile_code, profile_decorator, profile_file
 
 
 def test_profile_ctx(tmpdir):
@@ -21,6 +21,47 @@ def test_profile_code(tmpdir):
         code='for i in range(1000): 1 + 1',
         graph_filename=graph_file.strpath,
         view=False)
+    assert graph_file.isfile()
+
+
+def test_profile_decorator_args(tmpdir):
+    graph_file = tmpdir.join('profile_ctx.png')
+    assert not graph_file.isfile()
+
+    @profile_decorator(graph_file.strpath, view=False)
+    def foo():
+        for i in range(1000):
+            1 + 1
+
+    assert not graph_file.isfile()
+    foo()
+    assert graph_file.isfile()
+
+
+def test_profile_decorator_no_args(tmpdir, mocker):
+    graph_file = tmpdir.join('profile_ctx.png')
+
+    # Mock inner function to make default parameters what we need for test
+    from cprofile_graph import _profile_decorator
+    original = _profile_decorator
+
+    def mock_profile_decorator(*args, **kwargs):
+        kwargs['graph_filename'] = graph_file.strpath
+        kwargs['view'] = False
+        return original(*args, **kwargs)
+
+    mocker.patch('cprofile_graph._profile_decorator',
+                 mock_profile_decorator, )
+
+    assert not graph_file.isfile()
+
+    @profile_decorator
+    def foo():
+        for i in range(1000):
+            1 + 1
+
+    assert not graph_file.isfile()
+    foo()
     assert graph_file.isfile()
 
 
