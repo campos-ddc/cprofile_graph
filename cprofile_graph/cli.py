a5efda52e9f1fae821f382bc0ec0edd29281214b
Diogo de Campos <diogo@unity3d.com>
2017-03-06 16:43:43 +0100

    Fixed options and showing defaul values in help

--- cprofile_graph/cli.py
+++ cprofile_graph/cli.py
@@ -27,14 +27,16 @@ from cprofile_graph import (DEFAULT_COLOUR_NODES_BY_SELFTIME,
 @click.option(
     '--view/--no-view',
     default=DEFAULT_VIEW,
-    help='Automatically open output file after profiling.')
+    help='Automatically open output file after profiling.',
+    show_default=True)
 @click.option(
-    '--colour-nodes-by-selftime',
+    '--selftime/--no-selftime',
     default=DEFAULT_COLOUR_NODES_BY_SELFTIME,
     help='Colour nodes by self time, rather than by total time '
-    '(sum of self and descendants)')
+    '(sum of self and descendants).',
+    show_default=True)
 def main(filename, code, graph_filename, node_threshold, edge_threshold, view,
-         colour_nodes_by_selftime):
+         selftime):
 
     if filename:
         profile_file(
@@ -43,7 +45,7 @@ def main(filename, code, graph_filename, node_threshold, edge_threshold, view,
             node_threshold=node_threshold,
             edge_threshold=edge_threshold,
             view=view,
-            colour_nodes_by_selftime=colour_nodes_by_selftime)
+            colour_nodes_by_selftime=selftime)
 
     elif code:
         profile_code(
@@ -52,7 +54,7 @@ def main(filename, code, graph_filename, node_threshold, edge_threshold, view,
             node_threshold=node_threshold,
             edge_threshold=edge_threshold,
             view=view,
-            colour_nodes_by_selftime=colour_nodes_by_selftime)
+            colour_nodes_by_selftime=selftime)
 
 
 if __name__ == "__main__":
